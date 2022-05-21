3cc9b81e9dec2b6d1b86d477ec2235b2e4e00be9
Lucas-brb <105973099+Lucas-brb@users.noreply.github.com>
2022-05-21 13:42:56 +0200

    Update reconstruction_front_onde.py
    Calcul angle de déviation

--- reconstruction_front_onde.py
+++ reconstruction_front_onde.py
@@ -1,10 +1,18 @@
 from Exp_SH import *
-
+import numpy as np
+from math import sqrt, atan
 
 
 def calcule_angle_incident(dx,dy,d_focale):
     return atan(sqrt(dx*dx+dy*dy)/d_focale)
 
-def calcule_angles_incidents():
-    pass
+def calcule_angles_incidents(mes_exp,pas,d_focale):
+    angles = []
+    for l in mes_exp:
+        a1 = []
+        for e in l:
+            a1.append(calcule_angle_incident(e[0],e[1],d_focale))
+        angles.append(a1)
+    return angles
 
+print(calcule_angles_incidents(mesure_experience,pas,focale_experience))
