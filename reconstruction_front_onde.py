9731f993b3cbff210749da31f7e1effbcb912764
jacques <laffont.jacques@gmail.com>
2022-05-22 13:30:41 +0200

    A corriger.

--- reconstruction_front_onde.py
+++ reconstruction_front_onde.py
@@ -57,15 +57,14 @@ def calcule_angles_incidents(mes_exp,pas,d_focale):
     return angles
 
 def calcul_point_image(forme_lentille,i,j,pas,d_focale):
-    zxi1j = forme_lentille[i+1][j]
-    zxij = forme_lentille[i][j]
-    zxij1 = forme_lentille[i][j+1]
-    dx = (zxi1j - zxij)/pas
-    dy = (zxij1 - zxij)/pas
+    dx = (forme_lentille[i+1+1][j+1]-forme_lentille[i][j+1])/(pas*2)
+    dy = (forme_lentille[i+1][j+1+1]-forme_lentille[i+1][j])/(pas*2)
     vi = np.array((np.sqrt(1-dx*dx-dy*dy),dx,dy))
     OM = vi * d_focale / vi[0]
     return OM[1],OM[2]
 
+def calcule_points_image(forme_lentille,pas,d_focale):
+    return [[calcul_point_image(forme_lentille,i,j,pas,focale_experience) for i in range(len(forme_lentille)-2)] for j in range(len(forme_lentille[0])-2)]
 
 def calcule_cumulerreur(forme_lentille,mesure,pas,d_focale):
 
@@ -80,21 +79,14 @@ def calcule_cumulerreur(forme_lentille,mesure,pas,d_focale):
     #     plt.ion()
 
     s = 0
-    Y=[]
-    Z=[]
+
     for i in range(len(mesure)):
-        Y.append(i*pas)
-        X=[]
-        l=[]
         for j in range(len(mesure[0])):
-            X.append(j*pas)
             dx,dy = calcul_point_image(forme_lentille,i,j,pas,d_focale)
             ex = dx - mesure[i][j][0]
             ey = dy - mesure[i][j][1]
             e = np.sqrt(ex*ex+ey*ey)
             s += e
-            l.append(e)
-        Z.append(l)
     #print(Z,forme_lentille)
     # X,Y = np.meshgrid(X,Y)
     # Z = np.array(Z)
@@ -131,15 +123,22 @@ def optim(forme_lentille,mesure,pas,d_focale):
 #calcule_polynome(vi)
 #print(calcule_image_deformations(mesure_experience))
 mesure_experience = [[mesure_experience[i][j] for i in range(2)] for j in range(2)]
-forme_lentille = [[0 for i in range(len(mesure_experience[0])+1)] for j in range(len(mesure_experience)+1)]
-print(optim(forme_lentille,mesure_experience,pas,focale_experience))
+forme_lentille = [[0 for i in range(len(mesure_experience[0])+2)] for j in range(len(mesure_experience)+2)]
+#print(optim(forme_lentille,mesure_experience,pas,focale_experience))
+
+forme_lentille=[[1, 0,0, 0], [0.0,0, 0,0], [0, 0, 0,0],[0,0,0,0]];print(forme_lentille,calcule_points_image(forme_lentille,pas,focale_experience),mesure_experience)
+forme_lentille=[[0, 1,0, 0], [0.0,0, 0,0], [0, 0, 0,0],[0,0,0,0]];print(forme_lentille,calcule_points_image(forme_lentille,pas,focale_experience),mesure_experience)
+forme_lentille=[[0, 0,1, 0], [0.0,0, 0,0], [0, 0, 0,0],[0,0,0,0]];print(forme_lentille,calcule_points_image(forme_lentille,pas,focale_experience),mesure_experience)
+forme_lentille=[[0, 0,0, 1], [0.0,0, 0,0], [0, 0, 0,0],[0,0,0,0]];print(forme_lentille,calcule_points_image(forme_lentille,pas,focale_experience),mesure_experience)
+
+exit(0)
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 fig.show()
 Z = np.array([[np.sqrt(__[0]*__[0]+__[1]*__[1]) for __ in _]  for _ in mesure_experience])
-X = np.linspace(0,1,13)
-Y = np.linspace(0,1,11)
+X = np.linspace(0,1,len(mesure_experience))
+Y = np.linspace(0,1,len(mesure_experience[0]))
 X,Y = np.meshgrid(X,Y)
 ax.plot_surface(X,Y,Z,cmap = cm.coolwarm)
 plt.show()
